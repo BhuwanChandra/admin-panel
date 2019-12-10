3e38dd1cb6c8917e2c912d4903d60cbdd5b6aa9e
BhuwanChandra <bhuwan7779@gmail.com>
2019-12-10 20:39:26 +0530

    Updated

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,32 @@
+
+$(function () {
+
+  $(".progress").each(function () {
+
+    var value = $(this).attr('data-value');
+    var left = $(this).find('.progress-left .progress-bar');
+    var right = $(this).find('.progress-right .progress-bar');
+
+    if (value > 0) {
+      if (value <= 50) {
+        right.css('transform', 'rotate(' + percentageToDegrees(value) + 'deg)')
+      } else {
+        right.css('transform', 'rotate(180deg)')
+        left.css('transform', 'rotate(' + percentageToDegrees(value - 50) + 'deg)')
+      }
+    }
+
+  })
+
+  function percentageToDegrees(percentage) {
+
+    return percentage / 100 * 360
+
+  }
+
+});
+
+
 function makePie(id, data) {
     let myChart = document.getElementById(id).getContext('2d');
     let massPopChart = new Chart(myChart, {
